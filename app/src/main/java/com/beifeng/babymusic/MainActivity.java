d6ffa33355576dfbd61a73e5bb34e2c79d265250
beifeng <503745624@qq.com>
2017-04-26 14:37:30 +0800

    网络模块 rxjava+retrofit+okhttp引入

--- app/src/main/java/com/beifeng/babymusic/MainActivity.java
+++ app/src/main/java/com/beifeng/babymusic/MainActivity.java
@@ -2,11 +2,70 @@ package com.beifeng.babymusic;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.OnClick;
+import com.beifeng.babymusic.base.BaseActivity;
+import com.beifeng.babymusic.entity.GankEntity;
+import com.beifeng.babymusic.http.HttpFactory;
+import com.beifeng.babymusic.http.HttpResult;
+import com.beifeng.babymusic.http.HttpTransformer;
+import com.beifeng.babymusic.util.LogUtils;
+import com.beifeng.babymusic.util.ToastUtils;
+import java.util.List;
+import rx.Subscriber;
+import rx.functions.Action0;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity{
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+  private static final String TAG = "main";
+  @BindView(R.id.bt_loading)
+  Button bt_loading;
+  @BindView(R.id.bt_success)
+  Button bt_success;
+  @BindView(R.id.bt_fail)
+  Button bt_fail;
+
+  @Override protected int getLayoutId() {
+    return R.layout.activity_main;
   }
+
+  @Override protected void afterCreate(Bundle savedInstanceState) {
+
+  }
+
+  @OnClick({ R.id.bt_loading ,R.id.bt_success,R.id.bt_fail})
+  public void onViewClicked(View view) {
+    switch (view.getId()) {
+      case R.id.bt_loading:
+        HttpFactory.getHttpApiSingleton()
+            .getCategoryData("Android", 10, 1)
+            .compose(new HttpTransformer<HttpResult<List<GankEntity>>, List<GankEntity>>())
+            .doOnSubscribe(new Action0() {
+              @Override public void call() {
+                showLoadingView();
+              }
+            })
+            .subscribe(new Subscriber<List<GankEntity>>() {
+              @Override public void onCompleted() {
+                LogUtils.d(TAG, "Completed");
+                showContentView();
+              }
+
+              @Override public void onError(Throwable e) {
+                LogUtils.d(TAG, "OnError, Error is " + e.toString());
+                showErrorView();
+              }
+
+              @Override public void onNext(List<GankEntity> gankEntities) {
+
+              }
+            });
+        break;
+      case R.id.bt_success:
+        ToastUtils.showShort("成功");
+    }
+  }
+
 }
---
+++ app/src/main/java/com/beifeng/babymusic/http/HttpRetrofit.java
@@ -0,0 +1,62 @@
+package com.beifeng.babymusic.http;
+
+import com.beifeng.babymusic.App;
+import com.beifeng.babymusic.http.cookie.CookieManager;
+import com.beifeng.babymusic.http.interceptor.CacheInterceptor;
+import com.beifeng.babymusic.http.interceptor.LoggingInterceptor;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by ZHT on 2017/4/19.
+ * 初始化OkHttp和Retrofit
+ */
+
+public class HttpRetrofit {
+
+    private static final long DEFAULT_TIME_OUT = 12;
+
+    private final HttpApi httpApi;
+
+    private final static Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+            .serializeNulls()
+            .create();
+
+    HttpRetrofit() {
+
+        File cacheDir = new File(App.getApplication().getCacheDir(), "http");
+        Cache cache = new Cache(cacheDir, 10 * 1024 * 1024);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(DEFAULT_TIME_OUT, TimeUnit.SECONDS);
+        builder.readTimeout(DEFAULT_TIME_OUT, TimeUnit.SECONDS);
+        builder.writeTimeout(DEFAULT_TIME_OUT, TimeUnit.SECONDS);
+        builder.addNetworkInterceptor(new CacheInterceptor());
+        builder.addInterceptor(new CacheInterceptor());
+        builder.addInterceptor(new LoggingInterceptor());
+        builder.cache(cache);
+        builder.cookieJar(new CookieManager(App.getApplication()));
+        OkHttpClient client = builder.build();
+
+        Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
+        Retrofit retrofit = retrofitBuilder.baseUrl(HttpApi.BASE_URL)
+                .client(client)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        httpApi = retrofit.create(HttpApi.class);
+    }
+
+    public HttpApi getHttpApi() {
+        return httpApi;
+    }
+}
